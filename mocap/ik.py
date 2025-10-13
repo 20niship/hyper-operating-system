20842aad1dd1fa5292c6570865eb66ac9f92ff1d
20niship <idcnishimiya5378@gmail.com>
2025-10-13 12:05:20 +0900

    wip

---
+++ mocap/ik.py
@@ -0,0 +1,50 @@
+import pybullet as p
+import pybullet_data
+import numpy as np
+import time
+import math
+from scipy.spatial.transform import Rotation as R
+
+# p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -9.81)
+
+
+class URDFInverseKinematics:
+    def __init__(self, urdf_path, end_effector_link):
+        urdf_path = "./SO101/so101_new_calib.urdf"
+        self.robot = p.loadURDF(urdf_path, useFixedBase=True)
+        self.num_joints = p.getNumJoints(self.robot)
+        self.end_effector_link = 6
+
+        # 目標位置（円運動）
+        self.target_pos = [0.3, 0.1, 0.3]
+        self.target_quat = p.getQuaternionFromEuler([0, 0, 0])  # (roll, pitch, yaw)
+
+    def compute_ik(self, target_pos: list[float], target_quat: list[float]):
+        self.target_pos = target_pos
+        self.target_quat = target_quat
+
+        joint_angles = p.calculateInverseKinematics(
+            self.robot,
+            self.end_effector_link,
+            self.target_pos,
+            self.target_quat,
+            maxNumIterations=50,
+            residualThreshold=1e-4,
+        )
+
+        for i in range(p.getNumJoints(self.robot)):
+            if i >= len(joint_angles):
+                continue
+            p.setJointMotorControl2(
+                self.robot,
+                i,
+                p.POSITION_CONTROL,
+                targetPosition=joint_angles[i],
+                force=500,
+            )
+
+        p.addUserDebugLine([0, 0, 0], self.target_pos, [1, 0, 0], 3, lifeTime=0.1)
+        p.stepSimulation()
+        return joint_angles
--- tmp.py
+++
@@ -1,8 +0,0 @@
-from mocap.hand_tracking import Hand3DTracker
-
-tracker = Hand3DTracker(cam_l_idx=0, cam_r_idx=2)
-while True:
-    tracker._capture()
-    if not tracker.update():
-        break
-tracker.close()
